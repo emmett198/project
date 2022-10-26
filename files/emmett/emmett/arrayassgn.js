8e7315cb2292f554fb65ef3bf756e6cbb6eb6815
emmett198 <graceswaat352@gmail.com>
2022-10-26 11:54:41 +0100

    feat: creted new files

---
+++ files/emmett/emmett/arrayassgn.js
@@ -0,0 +1,7 @@
+//Declare an empty array
+let arr1=[]
+// Declaring my full name
+let fullName='Emmanuel mercy'
+// Spliting my full name
+
+
---
+++ files/emmett/emmett/emmanuel.js
@@ -0,0 +1,21 @@
+
+
+    const myName = 'Default';
+    myName = 'Chris';
+
+    let myAge = '42';
+
+    // Don't edit the code below here!
+
+    const section = document.querySelector('section');
+
+    const para1 = document.createElement('p');
+    const para2 = document.createElement('p');
+    para1.textContent = myName;
+    para2.textContent = `In 20 years, I will be ${myAge + 20}`;
+    section.appendChild(para1);
+    section.appendChild(para2);
+
+
+
+
